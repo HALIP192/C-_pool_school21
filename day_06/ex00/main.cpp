78a3f73e18533a3c8d3007d620191766ffe5e1c8
Neptune Titan <ntitan@at-n8.msk.21-school.ru>
2022-11-26 14:41:46 +0300

    day_06

--- day_06/ex00/main.cpp
+++ day_06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ntitan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 19:46:36 by ntitan            #+#    #+#             */
-/*   Updated: 2022/11/25 19:51:31 by ntitan           ###   ########.fr       */
+/*   Updated: 2022/11/26 14:39:09 by ntitan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
         double  dValue = strtod(argv[1], &pErr);
         std::string word(argv[1]);
 
-        if (word == "nan")
+        if (word == "nan" || word == "nanf")
         {
             std::cout << "char: impossible" << std::endl;
             std::cout << "int: impossible" << std::endl; 
@@ -36,18 +36,27 @@ int main(int argc, char **argv)
         {
             std::cout.precision(1);
             std::cout.setf(std::ios_base::fixed);
-            if (dValue > CHAR_MAX || dValue < 0)
-                std::cout << "char: impossible" << std::endl;
-            else if (isprint(static_cast<char>(dValue)))
-                std::cout << "char: '" << static_cast<char>(dValue) << "'" << std::endl;
-            else
-                std::cout << "char: Non displayable" << std::endl;
-            if (dValue > INT_MAX || dValue < INT_MIN)
+            if (word.size() == 1 && ((word[0] >= 32 && word[0] < 48) || 
+										 (word[0] > 57 && word[0] < 127))) {
+
+                std::cout << "char: '" << word << "'" << std::endl;
+                std::cout << "int: " << static_cast<int>(word[0]) << std::endl; 
+            	std::cout << "float: " << static_cast<float>(word[0]) << "f" << std::endl;
+            	std::cout << "double: " << static_cast<double>(word[0]) << std::endl;
+			} else {
+				if (dValue > CHAR_MAX || dValue < 0)
+					std::cout << "char: impossible" << std::endl;
+				else if (isprint(static_cast<char>(dValue)))
+					std::cout << "char: \'" << static_cast<char>(dValue) << "\'" << std::endl;
+				else
+					std::cout << "char: Non displayable" << std::endl;
+            	if (dValue > INT_MAX || dValue < INT_MIN)
                 std::cout << "int: impossible" << std::endl; 
-            else
-                std::cout << "int: " << static_cast<int>(dValue) << std::endl; 
-            std::cout << "float: " << static_cast<float>(dValue) << "f" << std::endl;
-            std::cout << "double: " << dValue << std::endl;
+            	else
+					std::cout << "int: " << static_cast<int>(dValue) << std::endl;
+				std::cout << "float: " << static_cast<float>(dValue) << "f" << std::endl;
+				std::cout << "double: " << static_cast<double>(dValue) << std::endl;
+			}
         }
     }
     else
